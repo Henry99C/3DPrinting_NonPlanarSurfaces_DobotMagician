4c26c775f9629e3e60ddd0f960ae20eaedc3ce64
Henry Carmona <108441910+Henry99C@users.noreply.github.com>
2024-02-14 18:06:52 -0500

    Update IK_Dobot_Full_Points.py

--- IK_Dobot_Full_Points.py
+++ IK_Dobot_Full_Points.py
@@ -1,59 +1,59 @@
-from ecuaciones_cinematica_inversa import CalcTheta1, CalcTheta2, CalcTheta3, CalcTheta5, CalcTheta6
-from filtro_angulos import valores_posibles, valores_posibles_J2_J3
+from inverse_kinematic_equations import CalcTheta1, CalcTheta2, CalcTheta3, CalcTheta5, CalcTheta6
+from angle_filter import possible_values, possible_values_J2_J3
 import os
 
-# Se inicializa una matriz vacá para almacenar coordenadas, ángulos y marcos
-coordenadas = []
-angulos = []
-marcos = []
+# An empty matrix is initialized to store coordinates, angles and frames.
+coordinates = []
+angles = []
+frames = []
 
-# Paremetros D-H
+# Parameters D-H
 l0 = 138
 l1 = 135
 l2 = 147
 l3 = 125.61
 l4 = 79.5
 
-# Se ingresa la ruta de los archivos
-carpeta_actual = os.path.dirname(__file__)
-tipo_impresion = os.path.join(carpeta_actual, 'data_non_planar')
-ruta_data = os.path.join(tipo_impresion, 'data8')
-ruta_output = os.path.join(ruta_data, 'output')
-nombre_archivo_entrada = os.path.join(ruta_output, 'coordenadasTransformadas_marcos.txt')
+# Enter the path to the files
+current_path = os.path.dirname(__file__)
+print_type = os.path.join(current_path, 'data_non_planar')
+data_path = os.path.join(print_type, 'data8')
+output_path = os.path.join(data_path, 'output')
+input_file_name = os.path.join(output_path, 'coordinatesTransformed_frames.txt')
 
-# Se almacenan las coordenadas y marcos en las matrices
-with open(nombre_archivo_entrada, 'r') as archivo_entrada:
-    for linea in archivo_entrada:
-        x, y, z, matriz_str = linea.strip().split(",", 3)
-        coordenadas.append([float(x), float(y), float(z)])
-        marcos.append(eval(matriz_str))
+# The coordinates and frames are stored in the matrices
+with open(input_file_name, 'r') as input_file:
+    for line in input_file:
+        x, y, z, matriz_str = line.strip().split(",", 3)
+        coordinates.append([float(x), float(y), float(z)])
+        frames.append(eval(matriz_str))
 
-# Se realiza el calculo de la cinematica pose a pose
-for i in range(0, len(coordenadas)):
-    x = coordenadas[i][0]
-    y = coordenadas[i][1]
-    z = coordenadas[i][2]
-    rd = marcos[i]
+# The calculation of the kinematics is performed pose by pose.
+for i in range(0, len(coordinates)):
+    x = coordinates[i][0]
+    y = coordinates[i][1]
+    z = coordinates[i][2]
+    rd = frames[i]
     try:
-        J6 = valores_posibles(CalcTheta6(rd), -85, 85)[0]
-        J5 = valores_posibles(CalcTheta5(rd), -20, 80)[0]
-        J1 = valores_posibles(CalcTheta1(x, y, l4, J6), -135, 135)[0]
-        J2, _, J3 = valores_posibles_J2_J3(CalcTheta2(x, z, l0, l1, l2, l3, l4, J1, J5, J6),
+        J6 = possible_values(CalcTheta6(rd), -85, 85)[0]
+        J5 = possible_values(CalcTheta5(rd), -20, 80)[0]
+        J1 = possible_values(CalcTheta1(x, y, l4, J6), -135, 135)[0]
+        J2, _, J3 = possible_values_J2_J3(CalcTheta2(x, z, l0, l1, l2, l3, l4, J1, J5, J6),
                                           CalcTheta3(x, z, l0, l1, l2, l3, l4, J1, J5, J6))
     except IndexError:
         print(i)
         print(f"{J6}, {J5}, {J3}, {J2}, {J1}")
         J6, J5, J3, J2, J1 = [None, None, None, None, None]
-        print("Error: No existe solucion para esta POSE.")
+        print("Error: There is no solution for this POSE")
 
     # print(f"Axis J1:{J1}, Axis J2:{J2}, Axis J3:{J3}, Axis J5:{J5}, Axis J6:{J6}")
-    angulos.append([J1, J2, J3, J5, J6])
+    angles.append([J1, J2, J3, J5, J6])
 
-# Ruta del archivo de salida
-nombre_archivo_salida = os.path.join(ruta_output, 'valores_angulos.txt')
+# Output file path
+output_file_name = os.path.join(output_path, 'angles_values.txt')
 
 # Se almacenan los angulos
-with open(nombre_archivo_salida, "w") as archivo_salida:
+with open(output_file_name, "w") as archivo_salida:
     archivo_salida.write("J1 J2 J3 J5 J6\n")
 
     for angulo in angulos:
