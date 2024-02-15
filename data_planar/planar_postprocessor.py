8301d09edbee224f26416147cfdb1fa224183b32
Henry Carmona <108441910+Henry99C@users.noreply.github.com>
2024-02-14 20:16:50 -0500

    Update planar_postprocessor.py

--- data_planar/planar_postprocessor.py
+++ data_planar/planar_postprocessor.py
@@ -2,101 +2,101 @@ import numpy as np
 import os
 from viewer_path import plot_planar_path
 
-# Se inicializa una matriz vacía para almacenar las coordenadas
-coordenadas = []
+# An empty matrix is initialized to store the coordinates
+coordinates = []
 
-# Se inicializa una variable para almacenar el valor de z de cada capa
-z_actual = None
+# A variable is initialized to store the z value of each layer
+z_current = None
 
-# Ruta completa al archivo G-code
-ruta_actual = os.path.join(os.path.dirname(__file__), 'data3')
-gcode_nombre = os.path.join(ruta_actual, 'custom_base_solid_09.gcode')
-ruta_output = os.path.join(ruta_actual, 'output')
+# Full path to G-code file
+current_path = os.path.join(os.path.dirname(__file__), 'data3')
+gcode_name = os.path.join(current_path, 'custom_base_solid_09.gcode')
+output_path = os.path.join(current_path, 'output')
 
 
-# Abrir el archivo G-code
-with open(gcode_nombre, "r") as archivo:
-    for linea in archivo:
-        palabras = linea.split()
-        #Revisa las lineas que contengan la palabra G0 o G1
-        if palabras and (palabras[0] == "G0" or palabras[0] == "G1"):
+# Open the G-code file
+with open(gcode_name, "r") as file:
+    for linea in file:
+        words = line.split()
+        # Check lines containing the word G0 or G1
+        if words and (words[0] == "G0" or words[0] == "G1"):
             x, y, z = None, None, None
-            for palabra in palabras:
-                # Encuentra las componentes X, Y, Z
-                if palabra.startswith("X"):
-                    x = float(palabra[1:])
-                elif palabra.startswith("Y"):
-                    y = float(palabra[1:])
-                elif palabra.startswith("Z"):
-                    if len(palabra) > 1:
-                        z = float(palabra[1:])
+            for word in words:
+                # Find the X, Y, Z components
+                if word.startswith("X"):
+                    x = float(word[1:])
+                elif word.startswith("Y"):
+                    y = float(word[1:])
+                elif word.startswith("Z"):
+                    if len(word) > 1:
+                        z = float(word[1:])
 
             if x is not None and y is not None:
                 if z is None:
-                    z = z_actual
-                coordenadas.append([x, y, z])
-                z_actual = z
+                    z = z_current
+                coordinates.append([x, y, z])
+                z_current = z
 
-nombre_archivo_salida = os.path.join(ruta_output, 'coordenadas.txt')
+output_file_name = os.path.join(output_path, 'coordinates.txt')
 
-# Se almacenan las coordenas en un archivo txt
-with open(nombre_archivo_salida, "w") as archivo_txt:
-    for coordenada in coordenadas:
-        archivo_txt.write(f"{coordenada[0]:.3f}, {coordenada[1]:.3f}, {coordenada[2]:.1f}\n")
+# The coordinates are stored in a txt file
+with open(output_file_name, "w") as txt_file:
+    for coordinate in coordinates:
+        txt_file.write(f"{coordinate[0]:.3f}, {coordinate[1]:.3f}, {coordinate[2]:.1f}\n")
 
-print(f"Las coordenadas han sido almacenadas en '{nombre_archivo_salida}'")
+print(f"The coordinates have been stored in: '{output_file_name}'")
 
-# Se inicializa una matriz vacía para almacenar las coordenadas transformadas
-coordenadas_transformadas = []
+# An empty matrix is initialized to store the transformed coordinates.
+coordinates_transformed = []
 
-# Se define la matriz de transformación BST (Slicer con respecto a la Base)
+# The BST (Slicer with respect to the Base) transformation matrix is defined 
 BST = np.array([[1, 0, 0, 79+100],
                 [0, 1, 0, -(79+28.5)],
                 [0, 0, 1, 32],
                 [0, 0, 0, 1]])
 
-for coordenada in coordenadas:
-    coordenada = [coordenada[0], coordenada[1], coordenada[2], 1]
-    coordenada_transformada = np.dot(BST, coordenada)
-    coordenadas_transformadas.append([coordenada_transformada[0], coordenada_transformada[1], coordenada_transformada[2]])
+for coordinate in coordinates:
+    coordinate = [coordinate[0], coordinate[1], coordinate[2], 1]
+    coordinate_transformed = np.dot(BST, coordinate)
+    coordinates_transformed.append([coordinate_transformed[0], coordinate_transformed[1], coordinate_transformed[2]])
 
-nombre_archivo_salida = os.path.join(ruta_output, 'coordenadasTransformadas.txt')
+output_file_name = os.path.join(output_path, 'coordinatesTransformed.txt')
 
-# Se almacenan las coordenadas transformadas en un archivo txt
-with open(nombre_archivo_salida, "w") as archivo_txt:
-    for coordenada in coordenadas_transformadas:
-        archivo_txt.write(f"{coordenada[0]:.3f}, {coordenada[1]:.3f}, {coordenada[2]:.1f}, [[0, 0, -1], [0, -1, 0], [-1, 0, 0]]\n")
+# The transformed coordinates are stored in a txt file
+with open(output_file_name, "w") as txt_file:
+    for coordinate in coordinates_transformed:
+        txt_file.write(f"{coordinate[0]:.3f}, {coordinate[1]:.3f}, {coordinate[2]:.1f}, [[0, 0, -1], [0, -1, 0], [-1, 0, 0]]\n")
 
-print(f"Las coordenadas transformadas han sido almacenadas en '{nombre_archivo_salida}'")
+print(f"The transformed coordinates have been stored in: '{output_file_name}'")
 
-# Se inicializa una matriz vacía para almacenar las coordenadas aumentadas
-coordenadas_aumentadas = []
+# An empty matrix is initialized to store the increased coordinates
+increased_coordinates = []
 
-for i in range(len(coordenadas_transformadas) - 1):
-    coordenadas_aumentadas.append(coordenadas_transformadas[i])
+for i in range(len(coordinates_transformed) - 1):
+    increased_coordinates.append(coordinates_transformed[i])
 
-    # Se calcula la distancia entre el punto actual y el siguiente
-    x1, y1, z1 = coordenadas_transformadas[i]
-    x2, y2, z2 = coordenadas_transformadas[i + 1]
-    distancia = ((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2) ** 0.5
+    # The distance between the current point and the next point is calculated
+    x1, y1, z1 = coordinates_transformed[i]
+    x2, y2, z2 = coordinates_transformed[i + 1]
+    distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2) ** 0.5
 
-    # Si la distancia es mayor a 5 mm se aumentan los puntos
-    if distancia > 5:
-        numero_puntos = int(distancia / 5)
-        paso = [(x2 - x1) / (numero_puntos + 1), (y2 - y1) / (numero_puntos + 1), (z2 - z1) / (numero_puntos + 1)]
+    # If the distance is greater than 5 mm, the points are increased
+    if distance > 5:
+        number_points = int(distancia / 5)
+        step = [(x2 - x1) / (number_points + 1), (y2 - y1) / (number_points + 1), (z2 - z1) / (number_points + 1)]
         for j in range(numero_puntos):
-            nueva_x = x1 + paso[0] * (j + 1)
-            nueva_y = y1 + paso[1] * (j + 1)
-            nueva_z = z1 + paso[2] * (j + 1)
-            coordenadas_aumentadas.append([nueva_x, nueva_y, nueva_z])
+            new_x = x1 + step[0] * (j + 1)
+            new_y = y1 + step[1] * (j + 1)
+            new_z = z1 + step[2] * (j + 1)
+            increased_coordinates.append([new_x, new_y, new_z])
 
-nombre_archivo_salida = os.path.join(ruta_output, 'coordenadasAumentadasTransformadas.txt')
+output_file_name = os.path.join(ruta_output, 'coordinatesIncreasedTransformed.txt')
 
-# Se almacenan las coordenadas aumentadas y transformadas en un archivo txt
-with open(nombre_archivo_salida, "w") as archivo_txt:
-    for coordenada in coordenadas_aumentadas:
-        archivo_txt.write(f"{coordenada[0]:.3f}, {coordenada[1]:.3f}, {coordenada[2]:.1f}, [[0, 0, -1], [0, -1, 0], [-1, 0, 0]]\n")
+# The increased and transformed coordinates are stored in a txt file
+with open(output_file_name, "w") as txt_file:
+    for coordinate in increased_coordinates:
+        txt_file.write(f"{coordinate[0]:.3f}, {coordinate[1]:.3f}, {coordinate[2]:.1f}, [[0, 0, -1], [0, -1, 0], [-1, 0, 0]]\n")
 
-print(f"Las coordenadas aumentadas han sido almacenadas en: '{nombre_archivo_salida}'")
+print(f"The augmented coordinates have been stored in: '{output_file_name}'")
 
-plot_planar_path(coordenadas_transformadas, coordenadas_aumentadas, type='scatter')
+plot_planar_path(coordinates_transformed, increased_coordinates, type='scatter')
